5d9681e7c3c14927b60a4867cb773db01a2f9c93
ManupaDev <manupadevsuite@gmail.com>
2025-10-12 22:12:18 +0530

    feat: client side data pipeline

--- src/pages/home/components/SolarEnergyProduction.jsx
+++ src/pages/home/components/SolarEnergyProduction.jsx
@@ -5,6 +5,7 @@ import EnergyProductionCards from "./EnergyProductionCards";
 import Tab from "./Tab";
 import { useEffect } from "react";
 import { useState } from "react";
+import { subDays, toDate, format } from "date-fns";
 import { useGetEnergyGenerationRecordsBySolarUnitQuery } from "@/lib/redux/query";
 
 const SolarEnergyProduction = () => {
@@ -32,18 +33,80 @@ const SolarEnergyProduction = () => {
   //   ? energyProductionData.filter((el) => el.hasAnomaly)
   //   : [];
 
-  const filteredEnergyProductionData = energyProductionData.filter((el) => {
+  const { data, isLoading, isError, error } =
+    useGetEnergyGenerationRecordsBySolarUnitQuery("68ebc456189fc937242ec221");
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!data || isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  const formattedData = data.map((el) => {
+    return {
+      ...el,
+      timestamp: toDate(el.timestamp),
+    };
+  });
+
+  const latestGenerationRecord = formattedData[0];
+  const sevenDaysAgo = subDays(latestGenerationRecord.timestamp, 6);
+
+  const filteredData = formattedData.filter((el) => {
+    return el.timestamp >= sevenDaysAgo;
+  });
+
+  const mappedData = filteredData.map((el) => {
+    return {
+      ...el,
+      date: format(el.timestamp, "yyyy-MM-dd"),
+    };
+  });
+
+  // console.log(mappedData);
+
+  const groupedData = {};
+
+  mappedData.forEach((el) => {
+    if (groupedData[el.date]) {
+      groupedData[el.date].push(el);
+    } else {
+      groupedData[el.date] = [];
+      groupedData[el.date].push(el);
+    }
+  });
+
+  // console.log(groupedData);
+
+  const groupedDataArray = Object.entries(groupedData);
+  // console.log(groupedDataArray);
+
+  const calculateTotalProduction = (data) => {
+    let total = 0;
+    data.forEach((el) => {
+      total += el.energyGenerated;
+    });
+    return total;
+  };
+
+  const newEnergyProductionData = groupedDataArray.map(([date, data]) => {
+    return {
+      day: format(toDate(date), "EEE"),
+      date: format(toDate(date), "MMM d"),
+      hasAnomaly: false,
+      production: calculateTotalProduction(data),
+    };
+  });
+
+  const filteredEnergyProductionData = newEnergyProductionData.filter((el) => {
     if (selectedTab === "all") {
       return true;
     } else if (selectedTab === "anomaly") {
       return el.hasAnomaly;
     }
   });
-
-  const { data, isLoading, isError, error } =
-    useGetEnergyGenerationRecordsBySolarUnitQuery("68e28831fe732ccfcd426e71");
-
-  console.log(data, isLoading);
 
   return (
     <section className="px-12 font-[Inter] py-6">
@@ -56,9 +119,6 @@ const SolarEnergyProduction = () => {
           return <Tab key={tab.value} tab={tab} />;
         })}
       </div>
-      {/* <div className="mt-4">
-        <Button onClick={handleGetData}>Get Data</Button>
-      </div> */}
       <EnergyProductionCards
         energyProductionData={filteredEnergyProductionData}
       />
