2f96bb93b3808d9bedc7d0c88700602f116695d1
ManupaDev <manupadevsuite@gmail.com>
2025-10-19 22:09:01 +0530

    feat: enhance AppSidebar with active link highlighting and refactor menu item rendering
    - Introduced SideBarTab component to manage individual sidebar menu items and highlight the active link based on the current route.
    - Updated sidebar group label to be a clickable link for improved navigation.
    - Cleaned up the rendering logic for sidebar menu items, enhancing code readability and maintainability.

--- src/components/AppSidebar.jsx
+++ src/components/AppSidebar.jsx
@@ -10,45 +10,58 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { useLocation } from "react-router";
+import { cn } from "@/lib/utils";
 
 // Menu items.
 const items = [
   {
     title: "Dashboard",
     url: "/dashboard",
-    icon: <LayoutDashboard className="w-8 h-8" size={32}/>,
+    icon: <LayoutDashboard className="w-8 h-8" size={32} />,
   },
   {
     title: "Anomalies",
     url: "/dashboard/anomaly",
-    icon: <TriangleAlert className="w-8 h-8" size={32}/>,
+    icon: <TriangleAlert className="w-8 h-8" size={32} />,
   },
-  { 
+  {
     title: "Analytics",
     url: "/dashboard/analytics",
-    icon: <ChartLine className="w-8 h-8" size={32}/>,
+    icon: <ChartLine className="w-8 h-8" size={32} />,
   },
 ];
+
+const SideBarTab = ({ item }) => {
+  let location = useLocation();
+  let isActive = location.pathname === item.url;
+
+  return (
+    <SidebarMenuItem key={item.url}>
+      <SidebarMenuButton asChild isActive={isActive}>
+        <Link
+          to={item.url}
+        >
+          {item.icon}
+          <span>{item.title}</span>
+        </Link>
+      </SidebarMenuButton>
+    </SidebarMenuItem>
+  );
+};
 
 export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel className="text-xl font-bold text-foreground">
-            Aelora
+          <SidebarGroupLabel className="text-3xl font-bold text-foreground">
+            <Link to="/">Aelora</Link>
           </SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu className="mt-4 text">
               {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <Link to={item.url}>
-                      {item.icon}
-                      <span>{item.title}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
+                <SideBarTab key={item.url} item={item} />
               ))}
             </SidebarMenu>
           </SidebarGroupContent>
